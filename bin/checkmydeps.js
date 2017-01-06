3a5fce3b53dfdd252b2ccc3ccc5bb46bbb1132e5
Aurelien Ribon <aurelien.ribon@gmail.com>
2017-01-06 16:36:17 +0100

    Generate executable report with new utils fonctions

--- bin/checkmydeps.js
+++ bin/checkmydeps.js
@@ -53,69 +53,26 @@ suspend.run(function* () {
 
 
 // -----------------------------------------------------------------------------
-// REPORT CREATION
+// HELPERS
 // -----------------------------------------------------------------------------
 
 function createReport(result) {
   let report = '';
 
   if (result.ok.length && !hideUpToDate) {
-    report += createTable(result.ok, 'UP-TO-DATE');
+    report += `UP-TO-DATE\n--------------------------------------------------------------------------------\n`;
+    report += utils.createReportTable(result.ok);
   }
 
   if (result.nok.length) {
-    report += createTable(result.nok, 'OUTDATED');
+    report += `\n\nOUTDATED\n--------------------------------------------------------------------------------\n`;
+    report += utils.createReportTable(result.nok);
   } else {
-    report += '\nAll dependencies are up-to-date!';
+    report += '\n\nAll dependencies are up-to-date!';
   }
 
-  return formatTable(report.trim());
+  return utils.formatReportTable(report.trim());
 }
-
-function createTable(deps, title) {
-  let table = `\n${title}\n--------------------------------------------------------------------------------\n`;
-
-  for (const { name, found, needs, type } of deps) {
-    let needs2 = needs;
-    if (type === 'github') { needs2 += ' (on github)'; }
-
-    table += `${name}|needs ${needs2}|found ${found}\n`;
-  }
-
-  return table;
-}
-
-function formatTable(text) {
-  const lines       = text.split('\n');
-  let maxLenghtCol1 = 0;
-  let maxLenghtCol2 = 0;
-
-  for (const line of lines) {
-    const idxSeparator1 = line.indexOf('|');
-    if (idxSeparator1 === -1) { continue; }
-    maxLenghtCol1 = Math.max(maxLenghtCol1, idxSeparator1);
-
-    const idxSeparator2 = line.indexOf('|', idxSeparator1 + 1);
-    if (idxSeparator2 === -1) { continue; }
-    maxLenghtCol2 = Math.max(maxLenghtCol2, idxSeparator2 - idxSeparator1 - 1);
-  }
-
-  for (let i = 0; i < lines.length; ++i) {
-    const parts = lines[i].split('|');
-    if (parts.length !== 3) { continue; }
-
-    const filler1 = new Array(maxLenghtCol1 - parts[0].length + 1).join(' ');
-    const filler2 = new Array(maxLenghtCol2 - parts[1].length + 1).join(' ');
-
-    lines[i] = parts[0] + filler1 + ' | ' + parts[1] + filler2 + ' | ' + parts[2];
-  }
-
-  return lines.join('\n');
-}
-
-// -----------------------------------------------------------------------------
-// CHECK FOR UPDATE
-// -----------------------------------------------------------------------------
 
 function* checkForUpdate() {
   const content        = yield utils.downloadGithubPackage('AurelienRibon/node-checkmydeps', null, $$());
